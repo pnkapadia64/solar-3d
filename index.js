048c210cd1cf758318455c72173c52fb54b3780a
Prashasti Kapadia <pnkapadia6@gmail.com>
2025-01-10 02:04:58 +0100

    Updates

--- index.js
+++
@@ -1,4 +0,0 @@
-import SolarSystem from "./src/solar-system.js";
-
-const canvas = document.getElementById("main");
-new SolarSystem(canvas);
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/solar-3d/",
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
-});
